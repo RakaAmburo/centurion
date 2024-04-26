edcaa0804756afc370cc2760003cfc505b998e81
RakaAmburo <pablopaparini@hotmail.com>
2024-04-26 15:48:08 +0200

    fix

--- testUtil.js
+++ testUtil.js
@@ -4,8 +4,8 @@ import validator from "./securityUtils.js";
 // console.log(token)
 // console.log(await validator.tokenIsNotValid(token))
 
-let prot = validator.getPayloadStructure("hola", validator.webSocketComType.inst);
+/* let prot = validator.getPayloadStructure("hola", validator.webSocketComType.inst);
 let message = prot.prepareToSend()
-console.log(message)
-console.log(await validator.protocolCheck(message))
-console.log(validator.protocolExtract(message).message)
+console.log(message) */
+console.log(await validator.protocolCheck(1))
+//console.log(validator.protocolExtract("VvL0w2hcwTD3DcELy0cGZHHLOJHjosDgDMZ5pPOToTLLectOOcHmHCcjeTDgDk8cp9tRK2OToTLRwPHvqdBTSWZuDTNTpPEvSJ8cqPHvpvD3V90gDkn8p9yvSJ5zw9XToTD7GsL4").message)
